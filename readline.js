a433c7e576c35cdc987f1da7adc3b23df764bf52
bintang <bintangpramudyafarandhi@gmail.com>
2022-11-25 10:33:24 +0700

    belajar menggunakan  validator

--- readline.js
+++ readline.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -5,13 +6,31 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const validator = require('validator');
+
 rl.question('What is your name?', (name) => {
-    console.log(`Thank you ${name}`);
-    rl.question('What is your email?', (email) => {
-        console.log(`Your email is ${email}`);
-        rl.question('What is your phone number?', (number) => {
-            console.log(`Your phone number is ${number}`)
+    rl.question('What is your email?', (email) =>{
+        rl.question('What is your phone number?', (number) =>{
+
+            console.log(`\nName : ${name}`)
+
+            //validator email
+            if (validator.isEmail(email) == false) {
+                console.log("Your email is incorrect")
+            }
+            else {
+                console.log(`Email : ${email}`);
+            }
+
+            //validator no. hp
+            if (validator.isMobilePhone(number,'id-ID') == false) {
+                console.log("Your phone number is incorrect")
+            }
+            else {
+                console.log(`Phone number : ${number}`)
+            }
+
             rl.close();
-        });
-    });
-});
+        })
+    })
+})
